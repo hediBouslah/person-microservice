2b0713781a4f58ddecf5fc09c949f1dd667b4cea
MohamedHedi <mohamedelhedi.bouslah@maibornwolff.de>
2022-07-11 15:27:49 +0200

    delete Country Enum + Formatting V1.0.0.0__create_tables.sql

--- src/main/java/com/microservicestutorial/usermanagement/persistence/Country.java
+++
@@ -1,5 +0,0 @@
-package com.microservicestutorial.usermanagement.persistence;
-
-public enum Country {
-    TUN, DE, FR, GB
-}
--- src/main/resources/db/migration/V1.0.0.0__create_tables.sql
+++ src/main/resources/db/migration/V1.0.0.0__create_tables.sql
@@ -1,22 +1,26 @@
 create sequence person_id_seq increment by 1 start with 1 cache 20;
 create sequence address_id_seq increment by 1 start with 1 cache 20;
 
-create table t_persons(
-                          id bigint DEFAULT nextval('person_id_seq') constraint t_persons_pkey primary key not null,
-                          first_name varchar(100),
-                          last_name varchar(100),
-                          phone_number varchar(10),
-                          date_of_birth date,
-                          department_id bigint,
-                          constraint t_persons_pkey unique (id)
+create table t_persons
+(
+    id            bigint DEFAULT nextval('person_id_seq')
+        constraint t_persons_pkey primary key not null,
+    first_name    varchar(100),
+    last_name     varchar(100),
+    phone_number  varchar(10),
+    date_of_birth date,
+    department_id bigint,
+    constraint t_persons_pkey unique (id)
 );
 
-create table t_addresses(
-                            id bigint default nextval('address_id_seq') constraint t_addresses_pkey primary key not null,
-                            street varchar(100),
-                            city varchar(100),
-                            country varchar(100),
-                            person_id bigint,
-                            constraint t_addresses_pkey unique (id),
-                            constraint fk_person_id foreign key (person_id) references t_persons
+create table t_addresses
+(
+    id        bigint default nextval('address_id_seq')
+        constraint t_addresses_pkey primary key not null,
+    street    varchar(100),
+    city      varchar(100),
+    country   varchar(100),
+    person_id bigint,
+    constraint t_addresses_pkey unique (id),
+    constraint fk_person_id foreign key (person_id) references t_persons
 );
